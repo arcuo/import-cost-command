15357d88bdb19b1adfc14929b6d0ad3e9e9c3bc3
Benjamin <benjamin.zachariae@gmail.com>
2023-03-06 21:12:27 +0100

    package version

--- packages/es-size/src/packageVersion.ts
+++ packages/es-size/src/packageVersion.ts
@@ -1,20 +1,54 @@
-import * as fs from "fs";
+import { existsSync } from "fs";
 import { dirname, join, parse, resolve } from "path";
 
+export class PackageVersionError extends Error {
+  type = "PackageVersionError";
+}
+
 /** Find package in node_modules and output version */
-export function getPackageVersion(packageName: string) {}
+export function getPackageVersion(
+  packageName: string,
+  currentFilePath: string
+) {
+  const packageFolder = getPackageFolder(packageName, currentFilePath);
+  const packageJsonPath = join(packageFolder, "package.json");
+  const packageJson = require(packageJsonPath);
+  return packageJson.version;
+}
 
-export function getPackageFolderFromNodeModules(packageName: string) {}
+/** Get the package folder from a file inside the project */
+export function getPackageFolder(packageName: string, currentFilePath: string) {
+  const nodeModulesPath = getNodeModulesFolderPath(currentFilePath);
+  let currentDir = nodeModulesPath;
 
-export function getNodeModulesFolderPath(currentFileName: string) {
-  let currentDir = dirname(currentFileName);
+  packageName.split("/").forEach((packagePart) => {
+    if (existsSync(join(currentDir, packagePart))) {
+      currentDir = join(currentDir, packagePart);
+    } else {
+      throw new PackageVersionError(
+        `Could not find package ${packageName} in node_modules`
+      );
+    }
+  });
+
+  return currentDir;
+}
+
+/** From a file inside a node project, find the node_modules folder for the project by iterating outwards from the file. */
+export function getNodeModulesFolderPath(filePath: string, maxIterations = 10) {
+  let currentDir = dirname(filePath);
   const { root } = parse(currentDir);
+  let iter = 1;
 
-  while (currentDir !== root) {
-    if (fs.existsSync(join(currentDir, "node_modules"))) {
-      return parse(currentDir);
+  while (currentDir !== root && iter < maxIterations) {
+    if (existsSync(join(currentDir, "node_modules"))) {
+      const path = parse(currentDir);
+      return join(path.dir, path.base, "node_modules");
     } else {
       currentDir = resolve(currentDir, "..");
+      iter++;
     }
   }
+
+  throw new PackageVersionError("Could not find node_modules folder");
 }
--- packages/es-size/test/packageVersion.test.ts
+++ packages/es-size/test/packageVersion.test.ts
@@ -1,9 +1,61 @@
-import { describe, test } from 'vitest';
-import { getNodeModulesFolderPath } from '../src/packageVersion';
+import { describe, expect, test } from "vitest";
+import {
+  getNodeModulesFolderPath,
+  getPackageFolder,
+  getPackageVersion,
+  PackageVersionError,
+} from "../src/packageVersion";
 
-describe('Test packageVersion functions', () => {
-  test('getNodeModulePath', () => {
-    const result = getNodeModulesFolderPath(__filename);
-    console.log('result:', result);
+describe("Test packageVersion functions", () => {
+  describe("Test getNodeModulesFolderPath", () => {
+    test("Get node_modules from this current file", () => {
+      const dir = getNodeModulesFolderPath(__filename);
+      if (dir) {
+        const endPath = dir.split("/").slice(-4).join("/");
+        expect(endPath).toBe(
+          "import-cost-command/packages/es-size/node_modules"
+        );
+      }
+    });
+
+    test("Get node_modules in same folder as tested file", () => {
+      const ezSizePath = __dirname + "/../package.json";
+      const dir = getNodeModulesFolderPath(ezSizePath);
+      if (dir) {
+        const endPath = dir.split("/").slice(-4).join("/");
+        expect(endPath).toBe(
+          "import-cost-command/packages/es-size/node_modules"
+        );
+      }
+    });
+
+    test("Exceeding max iterations should throw PackageVersionError", () => {
+      expect(() => getNodeModulesFolderPath(__filename, 0)).toThrowError(
+        PackageVersionError
+      );
+    });
+  });
+
+  describe("Test getPackageFolderFromNodeModules", () => {
+    test("Get package folder from a file", () => {
+      const packageFolder = getPackageFolder("@babel/parser", __filename);
+      const endPath = packageFolder.split("/").slice(-6).join("/");
+      expect(endPath).toBe(
+        "import-cost-command/packages/es-size/node_modules/@babel/parser"
+      );
+    });
+
+    test("Throw error if package cannot be found", () => {
+      expect(() =>
+        getPackageFolder("non-existent-package", __filename)
+      ).toThrowError(PackageVersionError);
+    });
+  });
+
+  describe("Test getPackageVersion", () => {
+    test("Get package version from a file", () => {
+      const packageVersion = getPackageVersion("@babel/parser", __filename);
+      expect(packageVersion).toMatch(/7.21.\d+/);
+    });
   });
 });
